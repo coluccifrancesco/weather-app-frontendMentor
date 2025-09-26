944d20685d065afad2606632d46cf2ddd498d070
coluccifrancesco <coluccif2002@gmail.com>
2025-09-26 02:57:43 +0200

    Add backend server setup and integrate geocoding functionality

---
+++ backend/server.js
@@ -0,0 +1,3 @@
+require('dotenv').config()
+const geocodingUrl = process.env.GEOCODING_URL;
+const limitAndKey = process.env.GEOCODING_LIMIT_AND_API_KEY;
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+// ! CREA UN SERVER BACKEND MOLTO RUDIMENTALE CHE PROTEGGA I DATI
+
 let userCityInput = document.getElementById("search-input-city");
 let cityInput;
+let cityData
 
 // Updating the cityInput value with users search value
 userCityInput.addEventListener('input', () => {
 
     cityInput = userCityInput.value
-    console.log(cityInput);
+    
+    if (cityInput.length > 3) {
+        
+        try{getCity(cityData)}
+        catch{console.error(error)}
+    }
+
+    console.log(cityData);
 });
 
 
-// Fetching a list of cities with cityInput and showing in page the first 6 results
 
+// Fetching a list of cities with cityInput and showing in page the first 6 results
+function getCity(input){
+    fetch(`${geocodingUrl}${cityInput}${limitAndKey}`)
+    .then(res => res.json())
+    .then(data => {
+        cityData = data
+    })
+}
 
 
 // * Saving the coordinates of the chosen city in two variables
